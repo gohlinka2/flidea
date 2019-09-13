043a4537a3ec8d67986deaf5248d272843b0d886
gohlinka2 <gohlinka2@gmail.com>
2019-09-13 14:17:56 +0200

    Created file

--- app/src/main/java/cz/hlinkapp/flidea/view_models/MainViewModel.kt
+++ app/src/main/java/cz/hlinkapp/flidea/view_models/MainViewModel.kt
@@ -1,12 +1,28 @@
 package cz.hlinkapp.flidea.view_models
 
+import Flight
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import cz.hlinkapp.flidea.repositories.MainRepository
+import cz.hlinkapp.flidea.data.repositories.MainRepository
+import cz.hlinkapp.gohlinka2_utils2.utils.RequestInfo
 import javax.inject.Inject
 
 class MainViewModel @Inject constructor(repository: MainRepository): ViewModel() {
 
     private val mMainRepository : MainRepository = repository
 
-    private val
+    private var mFlights : LiveData<List<Flight>>? = null
+
+    val flights : LiveData<List<Flight>>? get() = mFlights
+
+    val flightsStatus : LiveData<RequestInfo> get() = mMainRepository.flightsStatus
+
+
+    fun initFlights() {
+        if (mFlights == null) mFlights = mMainRepository.getFlights()
+    }
+
+    fun forceRefreshFlights() {
+        mFlights = mMainRepository.forceRefreshFlights()
+    }
 }
