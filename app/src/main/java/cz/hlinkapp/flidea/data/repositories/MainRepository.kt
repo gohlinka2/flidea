d6b25ddd2720d86092f102138451a0d98d594b66
gohlinka2 <gohlinka2@gmail.com>
2019-09-13 15:25:27 +0200

    Added docs

--- app/src/main/java/cz/hlinkapp/flidea/data/repositories/MainRepository.kt
+++ app/src/main/java/cz/hlinkapp/flidea/data/repositories/MainRepository.kt
@@ -7,6 +7,10 @@ import cz.hlinkapp.flidea.data.data_sources.server.ServerDataSource
 import cz.hlinkapp.gohlinka2_utils2.utils.RequestInfo
 import javax.inject.Inject
 
+/**
+ * A repository for handling data.
+ * This is the class that decides from which source the data will be loaded and handles its refreshing.
+ */
 class MainRepository @Inject constructor(
     dao: MainDao,
     serverDataSource: ServerDataSource
@@ -17,6 +21,10 @@ class MainRepository @Inject constructor(
 
     val flightsStatus: LiveData<RequestInfo> get() = mServerDataSource.flightsStatus
 
+    /**
+     * Returns a LiveData with flights from the DB. Also refreshes the data if the right conditions are met.
+     * You can observe the status of the network call using [flightsStatus].
+     */
     fun getFlights(): LiveData<List<Flight>>? {
         mServerDataSource.refreshFlights()
         return mMainDao.getFlightsForDisplay()
