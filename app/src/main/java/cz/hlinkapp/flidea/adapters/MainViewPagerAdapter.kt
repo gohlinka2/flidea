711581f90ed081bb2397dcdbc514ddcf7e94a03e
gohlinka2 <gohlinka2@gmail.com>
2019-09-23 11:11:05 +0200

    Simplified

--- app/src/main/java/cz/hlinkapp/flidea/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/cz/hlinkapp/flidea/adapters/MainViewPagerAdapter.kt
@@ -1,6 +1,5 @@
 package cz.hlinkapp.flidea.adapters
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
@@ -9,20 +8,9 @@ import cz.hlinkapp.flidea.fragments.FlideaFragment
 /**
  * A [androidx.viewpager.widget.ViewPager] adapter for the main screen. Contains a fixed number of 5 [FlideaFragment] fragment.
  */
-class MainViewPagerAdapter (fm: FragmentManager, private val appName: String): FragmentStatePagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+class MainViewPagerAdapter (fm: FragmentManager): FragmentStatePagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
 
-    override fun getPageTitle(position: Int): CharSequence? {
-        return appName
-    }
-
-    override fun getItem(position: Int): Fragment = when (position) {
-        0 -> FlideaFragment().apply { arguments = Bundle().apply { putInt(FlideaFragment.ARG_FRAG_INDEX,0) } }
-        1 -> FlideaFragment().apply { arguments = Bundle().apply { putInt(FlideaFragment.ARG_FRAG_INDEX,1) } }
-        2 -> FlideaFragment().apply { arguments = Bundle().apply { putInt(FlideaFragment.ARG_FRAG_INDEX,2) } }
-        3 -> FlideaFragment().apply { arguments = Bundle().apply { putInt(FlideaFragment.ARG_FRAG_INDEX,3) } }
-        4 -> FlideaFragment().apply { arguments = Bundle().apply { putInt(FlideaFragment.ARG_FRAG_INDEX,4) } }
-        else -> throw IllegalArgumentException()
-    }
+    override fun getItem(position: Int): Fragment = FlideaFragment.createInstance(position)
 
     override fun getCount(): Int = 5
 }
