5979b330bfc4af341e6861d58d1156ff828c5e27
gohlinka2 <gohlinka2@gmail.com>
2019-09-26 11:37:05 +0200

    Added footer functionality

--- app/src/main/java/cz/hlinkapp/flidea/activities/MainActivity.kt
+++ app/src/main/java/cz/hlinkapp/flidea/activities/MainActivity.kt
@@ -9,10 +9,14 @@ import cz.hlinkapp.flidea.R
 import cz.hlinkapp.flidea.di.FlideaApplication
 import cz.hlinkapp.flidea.fragments.FlideaFragment
 import cz.hlinkapp.flidea.utils.findBehavior
+import cz.hlinkapp.flidea.utils.getStartOfDayTimestamp
 import cz.hlinkapp.flidea.view_models.MainViewModel
 import cz.hlinkapp.gohlinka2_utils2.utils.RequestInfo
+import cz.hlinkapp.gohlinka2_utils2.utils.setGone
+import cz.hlinkapp.gohlinka2_utils2.utils.setVisible
 import kotlinx.android.synthetic.main.activity_main.*
 import ru.semper_viventem.backdrop.BackdropBehavior
+import java.util.*
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
@@ -56,17 +60,45 @@ class MainActivity : AppCompatActivity() {
             })
         }
 
+        viewModel.flights?.observe(this, Observer {
+            if (it.isNotEmpty()) {
+                val timestamp = it[0].display_day_timestamp
+                if (timestamp == getStartOfDayTimestamp()) {
+                    footerLayout.setGone()
+                } else {
+                    footerLayout.setVisible()
+                    val cal = Calendar.getInstance()
+                    cal.timeInMillis = timestamp
+                    footerTitle.text = getString(R.string.todays_flight_ideas_havent_been_downloaded_yet_showing_flideas_for,"${cal.get(Calendar.DAY_OF_MONTH)}/${cal.get(Calendar.MONTH) + 1}")
+                }
+            } else {
+                footerLayout.setVisible()
+                footerTitle.text = getString(R.string.data_not_saved)
+            }
+        })
+
         viewModel.flightsStatus.observe(this, Observer {requestInfo ->
-            if (requestInfo.isProcessing()) status?.text = "Downloading"
-            else requestInfo.requestResult.getContentIfNotHandled().let {
+            if (requestInfo.isProcessing()) {
+                footerLayout.setVisible()
+                status?.text = getString(R.string.downloading)
+            } else requestInfo.requestResult.getContentIfNotHandled().let {
                 when (it) {
                     RequestInfo.RequestResult.OK -> {
-                        status?.text = "OK"
-//                        viewPager.currentItem = 0
+                        footerLayout.setGone()
+                        status?.text = getString(R.string.download_ok)
                     }
-                    RequestInfo.RequestResult.FAILED -> status?.text = "Failed"
-                    RequestInfo.RequestResult.NO_INTERNET -> status?.text = "No internet"
-                    else -> status?.text = "Idle"
+                    RequestInfo.RequestResult.FAILED -> {
+                        footerLayout.setVisible()
+                        status?.text = getString(R.string.download_failed)
+                    }
+                    RequestInfo.RequestResult.NO_INTERNET -> {
+                        footerLayout.setVisible()
+                        status?.text = getString(R.string.no_internet)
+                    }
+                    else -> {
+                        footerLayout.setGone()
+                        status?.text = getString(R.string.idle)
+                    }
                 }
             }
         })
