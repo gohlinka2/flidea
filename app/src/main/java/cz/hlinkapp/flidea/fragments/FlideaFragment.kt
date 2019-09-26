10d0f1aded00203c6a7c6ff8cb11aaa12426abf6
gohlinka2 <gohlinka2@gmail.com>
2019-09-26 13:39:20 +0200

    Added init of the newly created route recycler adapter

--- app/src/main/java/cz/hlinkapp/flidea/fragments/FlideaFragment.kt
+++ app/src/main/java/cz/hlinkapp/flidea/fragments/FlideaFragment.kt
@@ -3,14 +3,18 @@ package cz.hlinkapp.flidea.fragments
 import android.os.Bundle
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.DownsampleStrategy
 import cz.hlinkapp.flidea.R
+import cz.hlinkapp.flidea.adapters.RouteRecyclerAdapter
 import cz.hlinkapp.flidea.contracts.ServerContract
 import cz.hlinkapp.flidea.di.FlideaApplication
+import cz.hlinkapp.flidea.model.Route
 import cz.hlinkapp.flidea.view_models.MainViewModel
 import cz.hlinkapp.gohlinka2_utils2.fragments.abstraction.BaseFragment
 import cz.hlinkapp.gohlinka2_utils2.utils.setGone
+import cz.hlinkapp.gohlinka2_utils2.utils.setLayoutManagerSafely
 import cz.hlinkapp.gohlinka2_utils2.utils.setVisible
 import kotlinx.android.synthetic.main.fragment_flidea.*
 import javax.inject.Inject
@@ -21,6 +25,8 @@ class FlideaFragment : BaseFragment() {
 
     private lateinit var viewModel : MainViewModel
     private var mIndex : Int = -1
+
+    private var mRouteRecyclerAdapter = RouteRecyclerAdapter()
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
@@ -41,6 +47,9 @@ class FlideaFragment : BaseFragment() {
     override fun initViews(savedInstanceState: Bundle?) {
         super.initViews(savedInstanceState)
 
+        routeRecycler.setLayoutManagerSafely(LinearLayoutManager(context))
+        routeRecycler.adapter = mRouteRecyclerAdapter
+
         emptySpaceLayout.setOnClickListener {
             //TODO: force refresh data
         }
@@ -56,9 +65,10 @@ class FlideaFragment : BaseFragment() {
                 price.text = flight.price.toString()
                 currency.text = flight.currency
                 Glide.with(destinationImage)
-                    .load(ServerContract.createImageUrl(flight.mapIdto))
+                    .load(ServerContract.createLocationImageUrl(flight.mapIdto))
                     .downsample(DownsampleStrategy.FIT_CENTER)
                     .into(destinationImage)
+                mRouteRecyclerAdapter.routes = ArrayList<Route>().apply { addAll(flight.route) }
             } else {
                 contentLayout.setGone()
                 emptySpaceLayout.setVisible()
