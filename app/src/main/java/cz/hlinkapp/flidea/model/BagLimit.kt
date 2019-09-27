08d45cd07e164497d1f6ecda210859af91bfd774
gohlinka2 <gohlinka2@gmail.com>
2019-09-27 17:14:46 +0200

    Added docs.

--- app/src/main/java/cz/hlinkapp/flidea/model/BagLimit.kt
+++ app/src/main/java/cz/hlinkapp/flidea/model/BagLimit.kt
@@ -15,6 +15,9 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 For support, please feel free to contact me at https://www.linkedin.com/in/syedabsar */
 
 
+/**
+ * A model class representing the bag limit object, containing allowed dimensions.
+ */
 data class BagLimit (
 
 	@SerializedName("hold_width") var hold_width : Int,
