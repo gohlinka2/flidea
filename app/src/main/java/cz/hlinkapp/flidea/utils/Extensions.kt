3881bce7c9c117761294969144cd29313f050e7c
gohlinka2 <gohlinka2@gmail.com>
2019-09-27 17:38:19 +0200

    Simplified

--- app/src/main/java/cz/hlinkapp/flidea/utils/Extensions.kt
+++ app/src/main/java/cz/hlinkapp/flidea/utils/Extensions.kt
@@ -30,10 +30,8 @@ fun getStartOfDayTimestamp() : Long {
  * From https://github.com/Semper-Viventem/Material-backdrop/blob/master/README.md
  */
 fun <T : CoordinatorLayout.Behavior<*>> View.findBehavior(): T = layoutParams.run {
-    if (this !is CoordinatorLayout.LayoutParams) throw IllegalArgumentException("View's layout params should be CoordinatorLayout.LayoutParams")
-
-    (layoutParams as CoordinatorLayout.LayoutParams).behavior as? T
-        ?: throw IllegalArgumentException("Layout's behavior is not current behavior")
+    require(this is CoordinatorLayout.LayoutParams) { "View's layout params should be CoordinatorLayout.LayoutParams" }
+    (layoutParams as CoordinatorLayout.LayoutParams).behavior as? T ?: throw IllegalArgumentException("Layout's behavior is not current behavior")
 }
 
 /**
