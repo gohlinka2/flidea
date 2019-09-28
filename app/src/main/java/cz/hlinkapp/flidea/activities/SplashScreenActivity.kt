baa85d0f8ea45a35576137f6ba1230cb278c10b4
gohlinka2 <gohlinka2@gmail.com>
2019-09-28 12:48:42 +0200

    Create file

---
+++ app/src/main/java/cz/hlinkapp/flidea/activities/SplashScreenActivity.kt
@@ -0,0 +1,11 @@
+package cz.hlinkapp.flidea.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
