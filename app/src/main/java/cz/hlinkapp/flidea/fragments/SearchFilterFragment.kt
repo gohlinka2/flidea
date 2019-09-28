6aff3c35bf3f5271b202a8c3ab5d7893005a3ec6
gohlinka2 <gohlinka2@gmail.com>
2019-09-28 23:43:30 +0200

    Created basic functionality.

--- app/src/main/java/cz/hlinkapp/flidea/fragments/SearchFilterFragment.kt
+++ app/src/main/java/cz/hlinkapp/flidea/fragments/SearchFilterFragment.kt
@@ -1,11 +1,63 @@
 package cz.hlinkapp.flidea.fragments
 
+import android.os.Bundle
 import cz.hlinkapp.flidea.R
+import cz.hlinkapp.flidea.di.FlideaApplication
+import cz.hlinkapp.flidea.model.SearchFilters
+import cz.hlinkapp.flidea.utils.SharedPrefHelper
 import cz.hlinkapp.gohlinka2_utils2.fragments.abstraction.BaseFragment
+import kotlinx.android.synthetic.main.fragment_search_filter.*
+import javax.inject.Inject
 
 class SearchFilterFragment : BaseFragment() {
+
     override val layoutId: Int
         get() = R.layout.fragment_search_filter
+
+    @Inject
+    lateinit var sharedPrefHelper: SharedPrefHelper
+
+    private lateinit var mSearchFilters : SearchFilters
+
+    override fun initDependencies(savedInstanceState: Bundle?) {
+        super.initDependencies(savedInstanceState)
+
+        (activity?.applicationContext as? FlideaApplication)?.getApplicationComponent()?.inject(this)
+    }
+
+    override fun initViews(savedInstanceState: Bundle?) {
+        super.initViews(savedInstanceState)
+
+        mSearchFilters = sharedPrefHelper.getSearchFilters()
+
+        departureAirport.text = mSearchFilters.airportName
+        numberOfPassengers.text = mSearchFilters.passengers.toString()
+
+        addButton.setOnClickListener {
+            val pax = mSearchFilters.passengers
+            if (pax + 1 <= 9) {
+                mSearchFilters.passengers++
+                numberOfPassengers.text = mSearchFilters.passengers.toString()
+                saveSearchFilters()
+            }
+        }
+
+        addButton.setOnClickListener {
+            val pax = mSearchFilters.passengers
+            if (pax - 1 >= 1) {
+                mSearchFilters.passengers--
+                numberOfPassengers.text = mSearchFilters.passengers.toString()
+                saveSearchFilters()
+            }
+        }
+    }
+
+    /**
+     * Saves the current search filters.
+     */
+    private fun saveSearchFilters() {
+        sharedPrefHelper.saveSearchFilters(mSearchFilters)
+    }
 
     companion object {
         const val TAG = "SearchFilterFragment"
