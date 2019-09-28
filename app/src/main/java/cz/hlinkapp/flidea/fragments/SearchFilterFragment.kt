9f70afbac081df57e18adebbbffb81c91ed41e7e
gohlinka2 <gohlinka2@gmail.com>
2019-09-28 16:36:32 +0200

    Create files

---
+++ app/src/main/java/cz/hlinkapp/flidea/fragments/SearchFilterFragment.kt
@@ -0,0 +1,9 @@
+package cz.hlinkapp.flidea.fragments
+
+import cz.hlinkapp.flidea.R
+import cz.hlinkapp.gohlinka2_utils2.fragments.abstraction.BaseFragment
+
+class SearchFilterFragment : BaseFragment() {
+    override val layoutId: Int
+        get() = R.layout.fragment_search_filter
+}
