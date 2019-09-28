6e5b9aa4f0da161e1385404b33ac4b5774f27e22
gohlinka2 <gohlinka2@gmail.com>
2019-09-28 12:50:34 +0200

    Added functionality and docs

--- app/src/main/java/cz/hlinkapp/flidea/activities/SplashScreenActivity.kt
+++ app/src/main/java/cz/hlinkapp/flidea/activities/SplashScreenActivity.kt
@@ -1,11 +1,16 @@
 package cz.hlinkapp.flidea.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * An activity whose only purpose is to display a splash screen before it initializes, and then continue to [MainActivity].
+ */
 class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        startActivity(Intent(this,MainActivity::class.java))
     }
 }
