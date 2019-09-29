9b1c3b13ab83a5f5f7b663e97b6e440a1961ccef
gohlinka2 <gohlinka2@gmail.com>
2019-09-29 21:36:24 +0200

    Removed 'invalidated' column

--- app/src/main/java/cz/hlinkapp/flidea/data/data_sources/room/MainDao.kt
+++ app/src/main/java/cz/hlinkapp/flidea/data/data_sources/room/MainDao.kt
@@ -1,6 +1,5 @@
 package cz.hlinkapp.flidea.data.data_sources.room
 
-import android.widget.Filter
 import androidx.lifecycle.LiveData
 import androidx.room.*
 import cz.hlinkapp.flidea.model.Flight
@@ -35,7 +34,7 @@ interface MainDao {
      * Loads LiveData with flights to be displayed in UI.
      * This loads 5 flights ordered by display_day_timestamp in descending order (flights selected for display on this or recent days have the highest timestamp).
      */
-    @Query("SELECT * FROM flight WHERE invalidated=0 ORDER BY display_day_timestamp DESC LIMIT 5")
+    @Query("SELECT * FROM flight ORDER BY display_day_timestamp DESC LIMIT 5")
     fun getFlightsForDisplay() : LiveData<List<Flight>>
 
     /**
@@ -43,7 +42,7 @@ interface MainDao {
      * This loads 5 flights that haven't been displayed yet, ordered by the time they were downloaded, then quality, then popularity, in descending order.
      * This should be called after new flights have been downloaded and saved to the db. The IDs returned from this should be then used to call [markFlightsForDisplay].
      */
-    @Query("SELECT id FROM flight WHERE display_day_timestamp =$DISPLAY_DAY_TIMESTAMP_DEF_VAL AND invalidated=0 ORDER BY fetched_timestamp,quality,popularity DESC LIMIT 5")
+    @Query("SELECT id FROM flight WHERE display_day_timestamp =$DISPLAY_DAY_TIMESTAMP_DEF_VAL ORDER BY fetched_timestamp,quality,popularity DESC LIMIT 5")
     fun selectFlightIdsForDisplay() : List<String>
 
     /**
