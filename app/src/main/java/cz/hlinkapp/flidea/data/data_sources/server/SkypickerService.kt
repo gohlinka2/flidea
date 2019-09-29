d84baa678c6c2b3cd7e9263fa3489ed59b7df38b
gohlinka2 <gohlinka2@gmail.com>
2019-09-29 18:02:27 +0200

    Added a function to search for flights

--- app/src/main/java/cz/hlinkapp/flidea/data/data_sources/server/SkypickerService.kt
+++ app/src/main/java/cz/hlinkapp/flidea/data/data_sources/server/SkypickerService.kt
@@ -1,6 +1,7 @@
 package cz.hlinkapp.flidea.data.data_sources.server
 
 import cz.hlinkapp.flidea.contracts.ServerContract
+import cz.hlinkapp.flidea.model.LocationSearchRootApiResponse
 import cz.hlinkapp.flidea.model.RootApiResponse
 import retrofit2.Call
 import retrofit2.http.GET
@@ -16,4 +17,10 @@ interface SkypickerService {
      */
     @GET(ServerContract.FLIGHTS)
     fun getFlights(@QueryMap params: @JvmSuppressWildcards Map<String,Any>) : Call<RootApiResponse>
+
+    /**
+     * Search locations including the query provided.
+     */
+    @GET(ServerContract.LOCATIONS)
+    fun getAirportSearchResults(@QueryMap params: @JvmSuppressWildcards Map<String, Any>) : Call<LocationSearchRootApiResponse>
 }
