e80dd34ab5d39e0fae2c9b3285c50b01d7c88c54
gohlinka2 <gohlinka2@gmail.com>
2019-09-29 14:47:54 +0200

    Added initViews

--- app/src/main/java/cz/hlinkapp/flidea/activities/PickAirportActivity.kt
+++ app/src/main/java/cz/hlinkapp/flidea/activities/PickAirportActivity.kt
@@ -3,11 +3,21 @@ package cz.hlinkapp.flidea.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import cz.hlinkapp.flidea.R
+import kotlinx.android.synthetic.main.activity_pick_airport.*
 
 class PickAirportActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pick_airport)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        setSupportActionBar(toolbar)
+
+        searchView.isSubmitButtonEnabled = true
+        searchView.setIconifiedByDefault(false)
     }
 }
