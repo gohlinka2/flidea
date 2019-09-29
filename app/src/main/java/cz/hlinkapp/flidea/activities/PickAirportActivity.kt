e7af9de1ca0c41ee3cdd862d87aa6ad296f66904
gohlinka2 <gohlinka2@gmail.com>
2019-09-29 14:25:50 +0200

    Created activity

---
+++ app/src/main/java/cz/hlinkapp/flidea/activities/PickAirportActivity.kt
@@ -0,0 +1,13 @@
+package cz.hlinkapp.flidea.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import cz.hlinkapp.flidea.R
+
+class PickAirportActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pick_airport)
+    }
+}
