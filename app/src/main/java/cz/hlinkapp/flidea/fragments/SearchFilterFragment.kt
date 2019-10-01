160fee4f7bc63e5c525cb53a41c4e84a1136a668
gohlinka2 <gohlinka2@gmail.com>
2019-10-01 14:10:57 +0200

    Simplified and added docs

--- app/src/main/java/cz/hlinkapp/flidea/fragments/SearchFilterFragment.kt
+++ app/src/main/java/cz/hlinkapp/flidea/fragments/SearchFilterFragment.kt
@@ -12,6 +12,11 @@ import cz.hlinkapp.gohlinka2_utils2.fragments.abstraction.BaseFragment
 import kotlinx.android.synthetic.main.fragment_search_filter.*
 import javax.inject.Inject
 
+/**
+ * A fragment which displays the current search filters and allows the user to change them.
+ * Designed to be displayed as a backdrop's back layer.
+ * @see {https://material.io/components/backdrop}
+ */
 class SearchFilterFragment : BaseFragment() {
 
     override val layoutId: Int
@@ -31,17 +36,16 @@ class SearchFilterFragment : BaseFragment() {
     override fun initViews(savedInstanceState: Bundle?) {
         super.initViews(savedInstanceState)
 
+        //load previously saved filters
         mSearchFilters = sharedPrefHelper.getSearchFilters()
-
-        departureAirport.text = mSearchFilters.airport.name
-        numberOfPassengers.text = mSearchFilters.passengers.toString()
+        updateViews()
 
         addButton.setOnClickListener {
             val pax = mSearchFilters.passengers
             if (pax + 1 <= 9) {
                 mSearchFilters.passengers++
-                numberOfPassengers.text = mSearchFilters.passengers.toString()
                 saveSearchFilters()
+                updateViews()
             }
         }
 
@@ -49,14 +53,12 @@ class SearchFilterFragment : BaseFragment() {
             val pax = mSearchFilters.passengers
             if (pax - 1 >= 1) {
                 mSearchFilters.passengers--
-                numberOfPassengers.text = mSearchFilters.passengers.toString()
                 saveSearchFilters()
+                updateViews()
             }
         }
 
-        departureAirport.setOnClickListener {
-            startActivityForResult(Intent(context,PickAirportActivity::class.java), PICK_AIRPORT_ACTION_CODE)
-        }
+        departureAirport.setOnClickListener { startActivityForResult(Intent(context,PickAirportActivity::class.java), PICK_AIRPORT_ACTION_CODE) }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -64,13 +66,21 @@ class SearchFilterFragment : BaseFragment() {
         if (requestCode == PICK_AIRPORT_ACTION_CODE && resultCode == Activity.RESULT_OK && data != null) {
             mSearchFilters.airport.name = data.getStringExtra(PickAirportActivity.KEY_AIRPORT_NAME)
             mSearchFilters.airport.id = data.getStringExtra(PickAirportActivity.KEY_AIRPORT_ID)
-            departureAirport.text = mSearchFilters.airport.name
             saveSearchFilters()
+            updateViews()
         }
     }
 
     /**
-     * Saves the current search filters.
+     * Sets the views to display current data from [mSearchFilters].
+     */
+    private fun updateViews() {
+        departureAirport.text = mSearchFilters.airport.name
+        numberOfPassengers.text = mSearchFilters.passengers.toString()
+    }
+
+    /**
+     * Saves the current search filters from [mSearchFilters] to shared prefs.
      */
     private fun saveSearchFilters() {
         sharedPrefHelper.saveSearchFilters(mSearchFilters)
@@ -79,6 +89,7 @@ class SearchFilterFragment : BaseFragment() {
     companion object {
         const val TAG = "SearchFilterFragment"
 
+        //Activity result code
         const val PICK_AIRPORT_ACTION_CODE = 12345
     }
 }
