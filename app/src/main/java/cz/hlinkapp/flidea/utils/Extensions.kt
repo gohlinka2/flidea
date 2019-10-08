6aa4fa2986bf01ec9b1387ef308fd89904f29281
gohlinka2 <gohlinka2@gmail.com>
2019-10-08 22:35:40 +0200

    Removed redundant file

--- app/src/main/java/cz/hlinkapp/flidea/utils/Extensions.kt
+++
@@ -1,6 +0,0 @@
-package cz.hlinkapp.flidea.utils
-
-/**
- * Formats this integer as a String with the given number of [digits] and with a leading zero.
- */
-fun Int.format(digits: Int): String = java.lang.String.format("%0${digits}d", this)
